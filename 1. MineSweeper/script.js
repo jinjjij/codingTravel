342a9a977e3e195d92fcb6d7b4fb6186b2e25558
윤진 <jinjjij0226@gmail.com>
2023-03-07 12:47:59 +0900

    functions all clear design aspects no immediate bomb function needed to be implemented

--- 1. MineSweeper/script.js
+++ 1. MineSweeper/script.js
@@ -15,6 +15,21 @@ class game{
         this.bombNum = parseInt(bombNum);
         this.board = this.createBoard(this.size_x, this.size_y);
         this._positionBomb();
+
+        // design
+        this.cellMargin = 2;
+        this.cellSize = 30;
+            // colors
+        this.backgroundColor = 'black';
+        this.boardColor1 = 'gray';
+        this.boardColor2 = 'black';
+        this.revealedColor = 'white';
+        // text colors : 1 blue · 2 green · 3 red · 4 dark blue · 
+        //               5 brown · 6 Cyan · 7 Black · 8 Grey.
+        this.textColor = ['#0000ff', '#008100', '#ff1300', '#000083', 
+                          '#810500', '#2a9494', '#000000', '#808080'];
+        
+        this.clickProtection = true;
     }
 
 
@@ -81,31 +96,33 @@ class game{
 
 
     _pressSurroundGrid(x,y){
-        this.pressGrid(x-1,y-1);
-        this.pressGrid(x-1,y);
-        this.pressGrid(x-1,y+1);
-        this.pressGrid(x,y-1);
-        this.pressGrid(x,y+1);
-        this.pressGrid(x+1,y-1);
-        this.pressGrid(x+1,y);
-        this.pressGrid(x+1,y+1);
+        this.pressGrid(x-1, y-1, false);
+        this.pressGrid(x-1, y  , false);
+        this.pressGrid(x-1, y+1, false);
+        this.pressGrid(x  , y-1, false);
+        this.pressGrid(x  , y+1, false);
+        this.pressGrid(x+1, y-1, false);
+        this.pressGrid(x+1, y  , false);
+        this.pressGrid(x+1, y+1, false);
     }
 
 
-    pressGrid(x,y){
+    pressGrid(x,y,bymouse){
         // check if position is out of board
         if(this._isoutofGrid(x,y)){
-            console.log("early return in pressGrid : outofGrid");
+            //console.log("early return in pressGrid : outofGrid");
             return;
-        }                  
+        }
 
         // if position is Flagged?
-        if(this.board[x][y].isFlaged == true)       return;
+        if(this.board[x][y].isFlaged == true){
+            return;
+        }
 
         // check if position is already revealed
         // -> if there is equal amount of flags around
         // -> reveal adjacent grids
-        if(this.board[x][y].isRevealed == true && this.board[x][y].num != 0){
+        if(bymouse && this.board[x][y].isRevealed == true && this.board[x][y].num != 0){
             console.log("revealed cell");
             let adjacentFlagCount = 0;
             for(let i=-1;i<2;i++)
@@ -119,21 +136,24 @@ class game{
             return;
         }
 
+        if(this.board[x][y].isRevealed == false){
+            // reveal grid
+            this.board[x][y].isRevealed = true;
 
-        // reveal grid
-        this.board[x][y].isRevealed = true;
+            // if its bomb -> gameover
+            if(this.board[x][y].num == -1){
+                // gameover!
+                console.log("GameOver!!");
+                gameOver();
+            }
 
-        // if its bomb -> gameover
-        if(this.board[x][y].num == -1){
-            // gameover!
-            console.log("GameOver!!");
+            // if empty -> reveal adjacent tiles
+            if(this.board[x][y].num == 0){
+                this._pressSurroundGrid(x,y);
+                console.log("empty!");
+            }
         }
-
-        // if empty -> reveal adjacent tiles
-        if(this.board[x][y].num == 0){
-            this._pressSurroundGrid(x,y);
-            console.log("empty!");
-        }
+        
 
         // else ( Grid is 1~8 )
         // just return nothing
@@ -160,48 +180,54 @@ class game{
 
 
     drawCell(x,y){
-        let xpos = cellMargin + x*(cellSize+cellMargin);
-        let ypos = cellMargin + y*(cellSize+cellMargin);
+        let cM = this.cellMargin;
+        let cS = this.cellSize;
+
+        let xpos = cM + x*(cS + cM);
+        let ypos = cM + y*(cS + cM);
+        let thisNum = this.board[x][y].num;
 
         stroke('black');
         fill('gray');
-        square (xpos, ypos, cellSize);
+        square (xpos, ypos, cS);
 
         if(curGame.board[x][y].isRevealed == true){
             stroke('black');
             fill('white');
-            square (xpos, ypos, cellSize);
+            square (xpos, ypos, cS);
         }
         
         
 
         fill('black');
         textAlign(0, 0);
-        textSize(cellSize*3/4);
+        textSize(cS*3/4);
 
-        if(this.board[x][y].num == -1){
-            text('B', xpos + cellSize/6, ypos + cellSize/6, cellSize, cellSize);
-        }else if(this.board[x][y].num != 0){
-            text(this.board[x][y].num, xpos + cellSize/6, ypos + cellSize/6, cellSize, cellSize);
+        if(thisNum == -1){
+            text('B', xpos + cS/6, ypos + cS/6, cS, cS);
+        }else if(thisNum != 0){
+            stroke(this.textColor[thisNum - 1])
+            text(thisNum, xpos + cS/6, ypos + cS/6, cS, cS);
         }
         
-        /*
+        
         if(curGame.board[x][y].isRevealed == false){
             stroke('black');
             fill('gray');
-            square (xpos, ypos, cellSize);
+            square (xpos, ypos, cS);
         }
-        */
+        
 
         if(curGame.board[x][y].isFlaged){
             stroke('black');
             fill('black');
-            text('F', xpos + cellSize/6, ypos + cellSize/6, cellSize, cellSize);
+            text('F', xpos + cS/6, ypos + cS/6, cS, cS);
         }
     }
 
 
     drawBoard(){
+        background(this.backgroundColor);
         for(var x=0;x<curGame.size_x; x++){
             for(var y=0;y<curGame.size_y; y++){
                 this.drawCell(x,y);
@@ -209,17 +235,47 @@ class game{
         }
     }
 
+
+    createCV(){
+        let hor = this.size_x * (this.cellMargin + this.cellSize) + this.cellMargin;
+        let ver = this.size_y * (this.cellMargin + this.cellSize) + this.cellMargin;
+
+        createCanvas(hor, ver);
+        background(this.backgroundColor);
+    }
+
+
+    mouseHandle(mX, mY, mB){
+        if(this.clickProtection){
+            this.clickProtection = false;
+            return;
+        }
+        let x = parseInt(mX/(this.cellSize + this.cellMargin));
+        let y = parseInt(mY/(this.cellSize + this.cellMargin));
+        
+        if(this._isoutofGrid(x,y)){
+            return;
+        }
+
+        if(mB == 'left'){
+            this.pressGrid(x, y, true);
+        }else if(mB == 'right'){
+            this.flagGrid(x,y);    
+        }
+        
+        draw();
+    }
+
 }
 
 let curGame = null;
-let cellSize = 30;
-let cellMargin = 5;
 let gameStatus;
 
 function initGame(x,y,b){
     gameStatus = "game";
     console.log("initGame!" + '('+x+','+y+','+b+')');
     curGame = new game(x,y,b);
+    curGame.createCV();
     draw();
 }
 
@@ -227,15 +283,20 @@ function initGame(x,y,b){
 function setup() {
     console.log("Page Loaded");
     gameStatus = "menu";
-    createCanvas(500, 500);
+    menu();
 }
 
 function draw() {
+    clear();
     if(gameStatus == "game" && curGame){
         curGame.drawBoard();
-    } 
-    else if(gameStatus == "menu"){
-        menu();
+    }else if(gameStatus == 'gameover'){
+        console.log("gameover in draw");
+        stroke('black');
+        fill('black');
+        textSize(100);
+        text("GAME OVER!",20,20,480,280);
+    }else{
     }
     noLoop();
 }
@@ -244,34 +305,35 @@ function draw() {
 function mouseReleased(){
     // 게임오버됐거나 게임 오브젝트가 없으면 exit
     if(curGame && gameStatus == "game"){
-        let x = parseInt(mouseX/(cellSize+cellMargin));
-        let y = parseInt(mouseY/(cellSize+cellMargin));
-        
-        console.log(mouseButton);
-        if(this._isoutofGrid){
-            return;
-        }
-
-        if(mouseButton == 'left'){
-            curGame.pressGrid(x, y);
-        }else if(mouseButton == 'right'){
-            console.log("flag! : "+x+", "+y);
-            // flag
-            curGame.flagGrid(x,y);
-            
-        }
-        
-        draw();
+        curGame.mouseHandle(mouseX, mouseY, mouseButton);
     }
 }
 
 
 function menu(){
+    clear();
     let inp_x = createInput("10", Number);
     let inp_y = createInput("10", Number);
     let inp_b = createInput("10", Number);
     let but   = createButton("Create Game");
-    but.mousePressed(func => initGame(inp_x.value(), inp_y.value(), inp_b.value()));
+    but.mousePressed(func => {
+        inp_x.remove();
+        inp_y.remove();
+        inp_b.remove();
+        but.remove();
+        initGame(inp_x.value(), inp_y.value(), inp_b.value());
+    });
+}
+
+
+function gameOver(){
+    gameStatus = "gameover";
+    draw();
+    let but = createButton("OK");
+    but.mousePressed(func => {
+        but.remove();
+        menu();
+    });
 }
 
 
