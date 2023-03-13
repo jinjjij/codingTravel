ffd289cccbde7a8046a6e4db039b471b1be4e331
Jin <jinjjij0226@gmail.com>
2023-03-14 04:40:16 +0900

    hirakata project init

--- 2. maps/hike/src/App.js
+++ 2. maps/hike/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import logo from './logo.svg';
+
+
+class route{
+
+}
+
+
+class node{
+
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <div className="black-nav">
         <div>메</div>
+      </div>
+
+      <div className="map">
+        <img src={logo} alt="logo"></img>
       </div>
       
     </div>
